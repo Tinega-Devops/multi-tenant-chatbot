96d123d5ed38ba30bb30ae7f76cd8a7ff0535ddb
Tinega-Devops <oncharijohannes@gmail.com>
2022-12-12 22:09:33 +0300

    added message component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import Message from './Message';
 
 class Chatbot extends Component {
 
@@ -46,11 +47,23 @@ class Chatbot extends Component {
         }
     };
 
+    renderMessages(returnedMessages) {
+        if (returnedMessages) {
+            return returnedMessages.map((message, i) => {
+                    return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+                }
+            )
+        } else {
+            return null;
+        }
+    }
+
     render() {
         return (
             <div style={{height: 400, width: 400, float: 'right'}}>
                 <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
+                    {this.renderMessages(this.state.messages)}
                     <input type="text"/>
                 </div>
             </div>
