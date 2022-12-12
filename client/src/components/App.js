6543fe9abe8808b136c400e7ef4aa49f1afb8a34
Tinega-Devops <oncharijohannes@gmail.com>
2022-12-13 01:59:06 +0300

    bug fixes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from './Header';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import Shop from './shop/Shop';
 import Chatbot from './chatbot/Chatbot';
-
 const App = () => (
     <div>
        <BrowserRouter>
-       <div className="container">
+           <div className="container">
                <Header />
+               <Routes>
                <Route exact path="/" component={Landing} />
                <Route exact path="/about" component={About} />
                <Route exact path="/shop" component={Shop} />
-               <Chatbot />
+               </Routes>
+             <Chatbot />
            </div>
        </BrowserRouter>
     </div>
 )
-
 export default App;
--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import axios from "axios";
+
+import Cookies from 'universal-cookie';
+import { v4 as uuid } from 'uuid';
+
 import Message from './Message';
 
-class Chatbot extends Component {
+const cookies = new Cookies();
 
+class Chatbot extends Component {
+    messagesEnd;
+    talkInput;
     constructor(props) {
         super(props);
-
+        // This binding is necessary to make `this` work in the callback
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this.state = {
             messages: []
         };
+        if (cookies.get('userID') === undefined) {
+            cookies.set('userID', uuid(), { path: '/' });
+        }
     }
+
     async df_text_query (queryText) {
         let says = {
             speaks: 'user',
@@ -21,7 +33,7 @@ class Chatbot extends Component {
             }
         }
         this.setState({ messages: [...this.state.messages, says]});
-        const res = await axios.post('/api/text',  {text: queryText});
+        const res = await axios.post('/api/text',  {text: queryText, userID: cookies.get('userID')});
 
         for (let msg of res.data.fulfillmentMessages) {
             says = {
@@ -32,21 +44,25 @@ class Chatbot extends Component {
         }
     };
 
-
     async df_event_query(eventName) {
 
-        const res = await axios.post('/api/event',  {event: eventName});
+        const res = await axios.post('/api/event',  {event: eventName, userID: cookies.get('userID')});
 
         for (let msg of res.data.fulfillmentMessages) {
             let says = {
                 speaks: 'bot',
                 msg: msg
             }
-
             this.setState({ messages: [...this.state.messages, says]});
         }
     };
-
+    componentDidMount() {
+        this.df_event_query('Welcome');
+    }
+    componentDidUpdate() {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+        this.talkInput.focus();
+    }
     renderMessages(returnedMessages) {
         if (returnedMessages) {
             return returnedMessages.map((message, i) => {
@@ -57,14 +73,22 @@ class Chatbot extends Component {
             return null;
         }
     }
-
+    _handleInputKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.df_text_query(e.target.value);
+            e.target.value = '';
+        }
+    }
     render() {
         return (
             <div style={{height: 400, width: 400, float: 'right'}}>
                 <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
                     {this.renderMessages(this.state.messages)}
-                    <input type="text"/>
+                    <div ref={(el) => { this.messagesEnd = el; }}
+                         style={{ float:"left", clear: "both" }}>
+                    </div>
+                    <input type="text" ref={(input) => { this.talkInput = input; }}  onKeyPress={this._handleInputKeyPress}  />
                 </div>
             </div>
         );
