a18bf28dffa2a73c2c1b226211289e94e99c00c3
Tinega-Devops <oncharijohannes@gmail.com>
2022-12-13 08:12:41 +0300

    bug fix

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter,Route} from "react-router-dom";
 import Header from './Header';
 import Landing from './pages/Landing';
 import About from './pages/About';
@@ -10,11 +10,9 @@ const App = () => (
        <BrowserRouter>
            <div className="container">
                <Header />
-               <Routes>
                <Route exact path="/" component={Landing} />
                <Route exact path="/about" component={About} />
                <Route exact path="/shop" component={Shop} />
-               </Routes>
              <Chatbot />
            </div>
        </BrowserRouter>
